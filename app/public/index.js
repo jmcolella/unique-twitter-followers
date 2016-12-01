02a48ac403ef8534401706e8ec3f11d75d4d2e95
John Colella <colella.john@gmail.com>
2016-12-01 12:40:26 -0500

    update existing index.js and containers with necessary Redux functionality

--- app/public/index.js
+++ app/public/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { twitterFollowersApp } from '../reducers/combine_reducers';
 import App from '../containers/App';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={ createStore( twitterFollowersApp ) }>
+    <App />
+  </Provider>,
   document.getElementById( 'app' )
 );
