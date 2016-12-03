2daadbd2f2dcb4ddc6ff188b7a36ece6e64dda60
John Colella <colella.john@gmail.com>
2016-12-02 22:12:18 -0500

    update index.js with routes from react-router

--- app/public/index.js
+++ app/public/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { twitterFollowersApp } from '../reducers/combine_reducers';
+import routes from '../config/routes';
 import App from '../containers/App';
 
 ReactDOM.render(
-  <Provider store={ createStore( twitterFollowersApp ) }>
-    <App />
-  </Provider>,
+  routes,
   document.getElementById( 'app' )
 );
