59f0f9f420f3a9fe30bd2ae6adf3ad5b7af15577
John Colella <colella.john@gmail.com>
2016-12-02 23:52:22 -0500

    add route for following lists

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,11 +4,16 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { twitterFollowersApp } from '../reducers/combine_reducers';
 import App from '../containers/App';
+import AddScreenNames from '../containers/AddScreenNames';
+import FollowingLists from '../containers/FollowingLists';
 
 const routes = (
   <Provider store={ createStore( twitterFollowersApp ) }>
     <Router history={ hashHistory }>
-      <Route path='/' component={ App } />
+      <Route path='/' component={ App }>
+        <IndexRoute component={ AddScreenNames } />
+        <Route path="/following_lists" component={ FollowingLists } />
+      </Route>
     </Router>
   </Provider>
 );
