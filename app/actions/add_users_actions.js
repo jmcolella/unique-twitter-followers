6e6c3c98c1aa32af08b814f7a6202be041dd1d88
John Colella <colella.john@gmail.com>
2016-12-02 23:52:07 -0500

    fix typo in add user actions file

--- app/actions/add_users_actions.js
+++ app/actions/add_users_actions.js
@@ -1,6 +1,6 @@
 import * as Constants from '../constants/add_users_constants';
 
-export function addUsers ( user1, user2 ) => {
+export function addUsers ( user1, user2 ) {
   return {
     type: Constants.ADD_SCREEN_NAMES,
     user1ScreenName: user1,
