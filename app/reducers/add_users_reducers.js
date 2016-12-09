1bfc866aa54ef9d5d1c201daee060f651081dc1b
John Colella <colella.john@gmail.com>
2016-12-08 22:02:21 -0500

    update redux store to handle logic for grabbing followers from Twitter api

--- app/reducers/add_users_reducers.js
+++ app/reducers/add_users_reducers.js
@@ -1,16 +1,14 @@
 import * as Constants from '../constants/add_users_constants';
 
 const initialState = {
-  user1: "",
-  user2: ""
+  users: []
 }
 
 const screenNames = ( state = initialState, action ) => {
   switch ( action.type ) {
     case Constants.ADD_SCREEN_NAMES:
       return Object.assign( {}, state, {
-        user1: action.user1ScreenName,
-        user2: action.user2ScreenName
+        users: [ action.user1ScreenName, action.user2ScreenName ]
       });
     default:
       return state
--- app/reducers/combine_reducers.js
+++ app/reducers/combine_reducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import screenNames from './add_users_reducers';
+import { following } from './following_reducers';
 
 export const twitterFollowersApp = combineReducers({
-  screenNames
+  screenNames,
+  following
 });
