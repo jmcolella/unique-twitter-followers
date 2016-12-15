40e55b2a9e6969129afbadf85e7f2e69f631a610
John Colella <colella.john@gmail.com>
2016-12-14 22:22:10 -0500

    add mapStateToProps and componentWillReceiveProps lifecycle method to handle logic of dispatching action for comparing following for users

--- app/containers/FollowingLists.js
+++ app/containers/FollowingLists.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchFollowing } from '../actions/following_actions';
+import { fetchFollowing, compareFollowing } from '../actions/following_actions';
 import Friend from '../components/Friend';
 
 class FollowingLists extends Component {
-  constructor() {
-    super()
-
-    this.state = {
-      user1: [],
-      user2: []
-    }
-  }
   componentDidMount() {
     const { store, dispatch } = this.context;
     const state = store.getState();
@@ -23,13 +15,14 @@ class FollowingLists extends Component {
     });
   }
   componentWillReceiveProps( nextProps ) {
+    const { store } = this.context;
     let user1 = nextProps.following[nextProps.screenNames.users[0]];
     let user2 = nextProps.following[nextProps.screenNames.users[1]];
-    if ( user1.isFetching === false && user2.isFetching === false ) {
-      this.setState({
-        user1: user1.following,
-        user2: user2.following
-      });
+    let user1CompareLength = nextProps.following.user1Compare.length;
+    let user2CompareLength = nextProps.following.user2Compare.length;
+
+    if ( user1.isFetching === false && user2.isFetching === false && user1CompareLength === 0 && user2CompareLength === 0 ) {
+      store.dispatch( compareFollowing( user1.following, user2.following ) );
     }
   }
   componentWillUnmount() {
@@ -37,22 +30,22 @@ class FollowingLists extends Component {
   }
   render() {
     const { store } = this.context;
-    const storeState = store.getState();
+    const state = store.getState();
     return (
       <div>
-        <h1>{storeState.screenNames.users[0]}</h1>
+        <h1>{ state.screenNames.users[0] }</h1>
         {
-          this.state.user1.map( (f, index) => {
+          state.following.user1Compare.map( ( f, index ) => {
             return <Friend
-                      id={ index }
+                      key={ index }
                       friend={ f } />
           })
         }
-        <h1>{storeState.screenNames.users[1]}</h1>
+        <h1>{ state.screenNames.users[1] }</h1>
         {
-          this.state.user2.map( (f, index) => {
+          state.following.user2Compare.map( ( f, index ) => {
             return <Friend
-                      id={ index }
+                      key={ index }
                       friend={ f } />
           })
         }
