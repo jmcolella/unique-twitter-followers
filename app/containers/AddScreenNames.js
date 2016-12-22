593441d97080048098e9f219cdef6d80bfde785e
John Colella <colella.john@gmail.com>
2016-12-21 23:33:56 -0500

    work on MVP for new landing page and AddScreenNames styling

--- app/containers/AddScreenNames.js
+++ app/containers/AddScreenNames.js
@@ -25,38 +25,59 @@ class AddScreenNames extends Component {
   render() {
     let input1, input2;
     return (
-      <div className='children-container'>
-        <div className='inputs-container'>
-          <input
-            id='user-1-input'
-            ref={
-              ( node1 ) => {
-                input1 = node1
-              }
-            }
-            type="text"
-            placeholder="first handle" />
+      <div className='container fifty-percent-width'>
 
-          <input
-            id='user-2-input'
-            ref={
-              ( node2 ) => {
-                input2 = node2
-              }
-            }
-            type="text"
-            placeholder="second handle" />
+        <div className='banner'>
+          <div className='banner-left'>
+            <i className="fa fa-twitter" aria-hidden="true"></i>
+            <p>UNIQTWEET</p>
+         </div>
+
+         <div className='banner-right'>
+           <p>john colella | <a href='https://github.com/jmcolella/unique-twitter-followers'>code</a></p>
+         </div>
         </div>
 
-        <div className='button-container'>
-          <button
-            onClick={
-              () => this._submitScreenNames( input1.value, input2.value )
-            }
-          >
-            COMPARE
-          </button>
+        <div className="header-container">
+          <h1>Two Twitter Handles</h1>
+          <p>One click away from some unqiue finds.</p>
         </div>
+
+        <div className='children-container'>
+          <div className='inputs-container'>
+            <input
+              id='user-1-input'
+              ref={
+                ( node1 ) => {
+                  input1 = node1
+                }
+              }
+              type="text"
+              placeholder="first handle" />
+
+            <input
+              id='user-2-input'
+              ref={
+                ( node2 ) => {
+                  input2 = node2
+                }
+              }
+              type="text"
+              placeholder="second handle" />
+          </div>
+
+          <div className='button-container'>
+            <button
+              onClick={
+                () => this._submitScreenNames( input1.value, input2.value )
+              }
+            >
+              COMPARE
+            </button>
+          </div>
+
+        </div>
+
       </div>
     )
   }
