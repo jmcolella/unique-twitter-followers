d5d3ba008d0df8757b6dce5ecf87a3f42ee18aab
John Colella <colella.john@gmail.com>
2017-01-04 20:00:52 -0500

    update karma and webpack prod config with appropriate loaders for font awesome

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,12 @@ module.exports = ( config ) => {
         loaders: [
           { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ['airbnb', 'es2015'] } },
           { test: /\.css?$/, loader: 'style-loader!css-loader' },
-          { test: /\.scss$/, loaders: [ 'style', 'css', 'sass' ] }
+          { test: /\.scss$/, loaders: [ 'style', 'css', 'sass' ] },
+          { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
+          { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+          { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+          { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+          { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
         ]
       },
       externals: {
